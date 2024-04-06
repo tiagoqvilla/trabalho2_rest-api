b3e1e5d4c4b2fde2909b60f6fe4656e97dd5aca6
Tiago Villa <tiago.villa@edu.pucrs.br>
2024-04-06 17:08:26 -0300

    Adiciona chamada das funções

--- requests.py
+++ requests.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+# TODO adicionar rota base
 BASE_URL = ''
 
 
@@ -14,5 +15,10 @@ def fetch_by_id(id):
     data = json.loads(res.content)
     print(data)
 
+# TODO adicionar outras requests
+
+
+if __name__ == '__main__':
+    fetch_all()
 
 
